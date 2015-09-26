7daaf4a72c1f747c774062119b418e8c7c19964b
Mrxxm <362190221@qq.com>
2015-09-26 16:48:59 +0800

    关闭键盘

--- IOS/01-加法计算器/01-加法计算器/ViewController.h
+++ IOS/01-加法计算器/01-加法计算器/ViewController.h
@@ -7,8 +7,26 @@
 //
 
 #import <UIKit/UIKit.h>
+/*
+ IBAction & IBOutlet
+ 
+ IB:Interface Builder // 界面的构造器,编辑器,界面搭建
+ 
+ 1> IBAction : 表示要(如果需要监听按钮的点击方法，可以使用IBAction，就可以与IB进行连线，连线之后，在点击按钮时，就会执行相应地方法)
+ 2> IBOutlet : 插座(如果需要获得/修改界面上控件的属性，使用IBoutlet，就可以与IB进行连线)
 
+
+*/
 @interface ViewController : UIViewController
+
+// 计算方法
+- (IBAction)compute;
+
+// 声明两个文本框属性
+@property (nonatomic, weak) IBOutlet UITextField *num1;
+@property (nonatomic, weak) IBOutlet UITextField *num2;
+
+@property (nonatomic, weak) IBOutlet UILabel *resultLabel;
 
 
 @end
--- IOS/01-加法计算器/01-加法计算器/ViewController.m
+++ IOS/01-加法计算器/01-加法计算器/ViewController.m
@@ -24,4 +24,37 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+// 计算方法的实现
+- (void)compute
+{
+
+    NSLog(@"compute");
+    // 1.取得界面上“两个文本框”的内容
+    NSString *n1 = self.num1.text;
+    NSString *n2 = self.num2.text;
+    
+    NSLog(@"%@ - %@", n1, n2);
+    
+    // 2.将内容相加//转成int类型后相加
+    int result = n1.intValue + n2.intValue;
+    
+    // 3.显示结果，显示在“右侧的标签”中
+    self.resultLabel.text = [NSString stringWithFormat:@"%d", result];
+    
+    // 关闭键盘
+    // 在所有的面向对象语言中，有一个原则:谁申请，谁释放
+    // 对应键盘就应该，谁打开，谁关闭。
+    // FirstResponder：光标所在的文本框
+    // resign: 辞去
+    
+    //[self.num1 resignFirstResponder]; // 光标辞去时关闭键盘
+    //[self.num2 resignFirstResponder];
+    
+    // 让视图关闭键盘，无论现在谁是第一响应者
+    // 参数表示是否强行关闭键盘
+    [self.view endEditing:YES];
+    
+}
+
 @end
