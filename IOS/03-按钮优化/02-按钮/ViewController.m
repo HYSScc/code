52f3a73bcb3f3d170305776ce8439e802c1d1ebe
Mrxxm <362190221@qq.com>
2015-09-26 23:36:59 +0800

    9.26 DAY1

--- IOS/03-按钮优化/02-按钮/ViewController.m
+++ IOS/03-按钮优化/02-按钮/ViewController.m
@@ -8,15 +8,55 @@
 
 #import "ViewController.h"
 
+// 枚举类型本质上就是整数，定义的时候，如果只指定第一个数值，后续的数值依次递增
+// 枚举类型是解决魔法数字比较常用的手法
+typedef enum {
+    kMovingTop = 10,
+    kMovingBottom,
+    kMovingLeft,
+    kMovingRight,
+} kMovingDir;
+
+// 宏定义
+#define kMovingDelta 20.0f
+
+
 @interface ViewController ()
 
-// 声明按钮对象
 @property (weak, nonatomic) IBOutlet UIButton *iconButton;
 
 
 @end
 
 @implementation ViewController
+
+// 优化方法，让重复代码只出现一次
+- (IBAction)move:(UIButton *)button
+{
+    
+    // 1.取出frame
+    CGRect frame = self.iconButton.frame;
+    
+    // 2.根据按钮的类型，判断移动的方向，再修改结构体的成员
+    // magic number魔法数字
+    switch (button.tag) {
+        case kMovingTop:        // 上
+            frame.origin.y -= kMovingDelta;
+            break;
+        case kMovingBottom:        // 下
+            frame.origin.y += kMovingDelta;
+            break;
+        case kMovingLeft:        // 左
+            frame.origin.x -= kMovingDelta;
+            break;
+        case kMovingRight:        // 右
+            frame.origin.x += kMovingDelta;
+            break;
+    }
+    
+    // 3.重新为对象的结构体属性赋值
+    self.iconButton.frame = frame;
+}
 
 
 
