b948be80237ebaf231958de8e4edb375caf3476f
Mrxxm <362190221@qq.com>
2015-09-26 23:13:04 +0800

    第一次优化

--- IOS/02-按钮/02-按钮/ViewController.m
+++ IOS/02-按钮/02-按钮/ViewController.m
@@ -10,18 +10,75 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIButton *iconButton;
+
+
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+
+
+
+
+
+
+
+
+
+
+// 向上移动
+- (IBAction)top:(id)sender
+{
+    // 修改位置可以用frame
+    // 在OC中，不允许直接修改对象的结构体属性的成员
+    // 但是允许修改结构体属性的成员
+   // self.iconButton.frame.origin.y = 20;
+    
+    // 如果要修改对象的结构体属性，需要三个步骤
+    // 1.取出frame
+    CGRect frame = self.iconButton.frame;
+    // 2.修改结构体的成员
+    frame.origin.y -= 20;
+    // 3.重新为对象的结构体属性赋值
+    self.iconButton.frame = frame;
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+// 向下移动
+- (IBAction)bottom:(id)sender
+{
+    // 1.取出frame
+    CGRect frame = self.iconButton.frame;
+    // 2.修改结构体的成员
+    frame.origin.y += 20;
+    // 3.重新为对象的结构体属性赋值
+    self.iconButton.frame = frame;
 }
+
+// 向左移动
+- (IBAction)left:(id)sender
+{
+    // 1.取出frame
+    CGRect frame = self.iconButton.frame;
+    // 2.修改结构体的成员
+    frame.origin.x -= 20;
+    // 3.重新为对象的结构体属性赋值
+    self.iconButton.frame = frame;
+}
+
+// 向右移动
+- (IBAction)right:(id)sender
+{
+    // 1.取出frame
+    CGRect frame = self.iconButton.frame;
+    // 2.修改结构体的成员
+    frame.origin.x += 20;
+    // 3.重新为对象的结构体属性赋值
+    self.iconButton.frame = frame;
+}
+
+
 
 @end
