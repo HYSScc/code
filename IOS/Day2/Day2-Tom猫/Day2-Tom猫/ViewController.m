7f0fcf417374e28d5284e022e0a92692730350bd
Mrxxm <362190221@qq.com>
2015-09-29 22:45:41 +0800

    Initial Commit

--- IOS/Day2/Day2-Tom猫/Day2-Tom猫/ViewController.m
+++ IOS/Day2/Day2-Tom猫/Day2-Tom猫/ViewController.m
@@ -9,31 +9,19 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-@property (weak, nonatomic) IBOutlet UIImageView *tom;
 
 @end
 
 @implementation ViewController
 
-- (IBAction)eatBird
-{
-    // 序列帧动画 -> 顺序播放一组图片
-    
-    // 动画图片的数组
-    NSMutableArray *arrayM = [NSMutableArray array];
-    
-    // 添加动画播放的图片
-    for (int i = 0; i < 40; i++) {
-        // 图像名称
-        NSString *imageName = [NSString stringWithFormat:@"eat_%02d.jpg", i];
-        UIImage *image = [UIImage imageNamed:imageName];
-        [arrayM addObject:imageName];
-        NSLog(@"%@", imageName);
-    }
-    self.tom.animationImages = arrayM;
-    
-    // 开始动画
-    [self.tom startAnimating];
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 
 @end
--- IOS/Day2/Day2-Tom猫2/Day2-Tom猫2/ViewController.m
+++ IOS/Day2/Day2-Tom猫2/Day2-Tom猫2/ViewController.m
@@ -9,19 +9,68 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *tom;
 
 @end
 
 @implementation ViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+/*
+ 重构
+ 
+ 
+*/
+- (IBAction)knockout
+{
+    // 动画图片的数组
+    NSMutableArray *arrayM = [NSMutableArray array];
+    
+    // 添加动画播放的图片
+    for (int i = 0; i < 40; i++) {
+        // 图像名称
+        NSString *imageName = [NSString stringWithFormat:@"knock_%02d.jpg", i];
+        UIImage *image = [UIImage imageNamed:imageName];
+        [arrayM addObject:image];
+        NSLog(@"%@", imageName);
+    }
+    
+    // 设置动画数组
+    self.tom.animationImages = arrayM;
+    // 重复一次
+    self.tom.animationRepeatCount = 1;
+    // 动画时长
+    self.tom.animationDuration = self.tom.animationImages.count * 075;
+    
+    // 开始动画
+    [self.tom startAnimating];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)eatBird
+{
+    // 序列帧动画 -> 顺序播放一组图片
+    
+    // 动画图片的数组
+    NSMutableArray *arrayM = [NSMutableArray array];
+    
+    // 添加动画播放的图片
+    for (int i = 0; i < 40; i++) {
+        // 图像名称
+        NSString *imageName = [NSString stringWithFormat:@"eat_%02d.jpg", i];
+        UIImage *image = [UIImage imageNamed:imageName];
+        [arrayM addObject:image];
+        NSLog(@"%@", imageName);
+    }
+    
+    // 设置动画数组
+    self.tom.animationImages = arrayM;
+    // 重复一次
+    self.tom.animationRepeatCount = 1;
+    // 动画时长
+    self.tom.animationDuration = self.tom.animationImages.count * 075;
+    
+    // 开始动画
+    [self.tom startAnimating];
 }
+
 
 @end
