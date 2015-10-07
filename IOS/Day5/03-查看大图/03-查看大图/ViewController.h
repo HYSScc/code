f42ffbd7d9cbabb8365301f08dc4a731e7f7abdd
Mrxxm <362190221@qq.com>
2015-10-07 15:36:13 +0800

    Initial Commit

---
+++ IOS/Day5/03-查看大图/03-查看大图/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  03-查看大图
+//
+//  Created by tom-pc on 15/10/7.
+//  Copyright (c) 2015年 tom-pc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IOS/Day5/03-查看大图/03-查看大图/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  03-查看大图
+//
+//  Created by tom-pc on 15/10/7.
+//  Copyright (c) 2015年 tom-pc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
